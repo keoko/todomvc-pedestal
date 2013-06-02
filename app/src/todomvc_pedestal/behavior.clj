6c331c25a2be5af942d1e2990360f0b83673db56
keoko <natxo.cabre@gmail.com>
2013-06-02 18:40:23 +0200

    changed README.org for checking github

--- app/src/todomvc_pedestal/behavior.clj
+++ app/src/todomvc_pedestal/behavior.clj
@@ -2,19 +2,8 @@
     (:require [clojure.set :as set]
               [clojure.string :as string]
               [io.pedestal.app.util.log :as log]
-              [io.pedestal.app.messages :as msg]))
-
-(defn get-uuid
-  "returns a type 4 random UUID: xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx.
-   @code from http://catamorphic.wordpress.com/2012/03/02/generating-a-random-uuid-in-clojurescript/"
-  []
-  (let [r (repeatedly 30 (fn [] (.toString (rand-int 16) 16)))]
-    (apply str (concat (take 8 r) ["-"]
-                       (take 4 (drop 8 r)) ["-4"]
-                       (take 3 (drop 12 r)) ["-"]
-                       [(.toString  (bit-or 0x8 (bit-and 0x3 (rand-int 15))) 16)]
-                       (take 3 (drop 15 r)) ["-"]
-                       (take 12 (drop 18 r))))))
+              [io.pedestal.app.messages :as msg]
+              [todomvc-pedestal.data :as data]))
 
 
 (defn diff-by [f new old]
@@ -24,81 +13,65 @@
     (filter (comp new-keys f) new)))
 
 
-(defn test-state []
-  {:todos 
-   [{:id 1 :title "test 1" :completed false}
-    {:id 2 :title "test 2" :completed true}
-    {:id 3 :title "test 3" :completed true}]})
-
-(defn init-state []
-  (let [state (.getItem js/localStorage local-storage-name)]
-    (if (not state)
-      {:todos []}
-      (let [todos (keywordize-keys (js->clj (.parse js/JSON state)))]
-        {:todos todos}))))
-
-
 (defn todo-transform [state message]
-  (condp = (msg/type message)
-    msg/init (do
-               (.log js/console "init:" (:value message) state)
-               (:value message))
-    :add-todo (do
-                (.log js/console "value:" (:value message) (pr-str state))
-                (update-in state [:todos] conj 
-                           {:id (get-uuid)
-                            :title (:value message)
-                            :completed false}))
-    :del-todo (do
-                (let [id (:value message)]
-                  (update-in state [:todos] (fn [todos] (remove #(= id (:id  %)) todos)))))
-    :upd-todo (do
-                (let [{:keys [id title completed]} message]
-                  (.log js/console "upd-todo:" id title completed)
-                  (update-in state [:todos] (fn [todos] (map #(if ( = id (:id %)) (assoc % :title title :completed completed) %) todos)))))))
+  (let [new-state (condp = (msg/type message)
+                    msg/init (:value message)
+                    :add-todo (data/add-todo state (:value message))
+                    :del-todo (data/delete-todo state (:value message))
+                    :upd-todo (let [{:keys [id title completed]} message]
+                                (data/update-todo state id title completed))
+                    :clear-completed (data/clear-todo-completed state)
+                    :toggle-all (data/toggle-all-todo state (:completed message)))]
+    (.log js/console "data:" (data/update-stats new-state))
+    (data/update-stats new-state)))
 
 
 (defn new-deltas [value] 
-  (comment log/debug :in :new-deltas :new-value (pr-str value))
-  (.log js/console "new-deltas")
   (vec (mapcat (fn [{:keys [id] :as todo}] 
-                 (log/debug :in :inner-new-deltas :new-value id)
                  [[:node-create [:app :todos id] :map]
                   [:value [:app :todos id] todo]])
                value)))
 
 
 (defn- delete-deltas [value]
-  (comment log/debug :in :delete-deltas :value value)
   (vec (mapcat (fn [{:keys [id] :as todo}]
                  [[:node-destroy [:app :todos id]]])
                value)))
 
 (defn- update-deltas [value]
-  (comment log/debug :in :update-deltas :value value)
   (vec (mapcat (fn [{:keys [id] :as todo}]
                  [[:value [:app :todos id] todo]])
                value)))
 
 
+(defn- create-stats-deltas [value]
+  (when value
+    [[:node-create [:app :stats] :map]
+     [:value [:app :stats] value]]))
+
+(defn- destroy-stats-deltas [value]
+  (when value
+    [[:node-destroy [:app :stats]]]))
+
+(defn- update-stats-deltas [value]
+  (when  value
+    [[:value [:app :stats] value]]))
+
+
 (defn new-todos [state input-name old-input new-input]
   (let [old (:todos old-input)
         new (:todos new-input)]
-    (.log js/console "new-todos"  (diff-by :id new old))
-    (comment log/debug :new-todos true :diff)
     (diff-by :id new old)))
 
 
 (defn deleted-todos [state input-name old-input new-input]
   (let [old (:todos old-input)
         new (:todos new-input)]
-    (comment log/debug :in :deleted-todos :diff (diff-by :id old new) (pr-str old) (pr-str new2))
     (diff-by :id old new)))
 
 (defn updated-todos [state input-name old-input new-input]
   (let [old (:todos old-input)
         new (:todos new-input)]
-    (comment log/debug :in :updated-todos :diff (diff-by :id old new) (pr-str old) (pr-str new2))
     (for [n new 
           o old
           :when (and (= (:id n) (:id o))
@@ -106,23 +79,47 @@
                          (not= (:completed n) (:completed o))))]
       n)))
 
+
+(defn create-stats [state input-name old-input new-input]
+  (let [old (:stats old-input)
+        new (:stats new-input)]
+    (when (and (= 0 (:total-todo old))
+               (< 0 (:total-todo new)))
+      new)))
+
+
+(defn destroy-stats [state input-name old-input new-input]
+  (let [old (or (:stats old-input) {:total-todo 0})
+        new (:stats new-input)]
+    (when (and (= 0 (:total-todo new))
+               (< 0 (:total-todo old)))
+      new)))
+
+
+(defn update-stats [state input-name old-input new-input]
+  (let [old (:stats old-input)
+        new (:stats new-input)]
+    (when (and (< 0 (:total-todo new))
+               (< 0 (:total-todo old))
+               (not= (:todo-left new)
+                     (:todo-left old)))
+      new)))
+
+
 (defn todo-emit
-  ([inputs]
-     (let [changed-inputs (assoc-in inputs [:new-todos :old] [])]
-       (.log js/console "todo-emit-1" (pr-str changed-inputs))
-       (concat  (todo-emit inputs changed-inputs)
-                [[:node-create [:app] :map] [:node-create [:app :todos] :map]])))
+  ([inputs] initial-app-model)
   ([inputs changed-inputs]
-     (.log js/console "todo-emit" (pr-str changed-inputs))
+     (log/debug :in :todo-emit :changed-inputs (pr-str changed-inputs))
      (reduce (fn 
                [a input-name]
                (let [new-value (:new (get inputs input-name))]
-                 (comment log/debug :in :reduce :input input-name :new-value new-value)
-                 (.log js/console "in reduce")
                  (concat a (case input-name
                             :new-todos (new-deltas new-value)
                             :deleted-todos (delete-deltas new-value)
                             :updated-todos (update-deltas new-value)
+                            :create-stats (create-stats-deltas new-value)
+                            :destroy-stats (destroy-stats-deltas new-value)
+                            :update-stats (update-stats-deltas new-value)
                             []))))
              []
              changed-inputs)))
@@ -133,8 +130,12 @@
 
 
 (def todo-app
-  {:transform {:todo {:init (test-state) :fn todo-transform}}
+  {:transform {:todo {:init {:todos []} :fn todo-transform}}
    :combine {:new-todos {:fn new-todos :input #{:todo}}
              :deleted-todos {:fn deleted-todos :input #{:todo}}
-             :updated-todos {:fn updated-todos :input #{:todo}}}
-   :emit {:emit {:fn todo-emit :input #{:new-todos :deleted-todos :updated-todos}}}})
+             :updated-todos {:fn updated-todos :input #{:todo}}
+             :create-stats {:fn create-stats :input #{:todo}}
+             :destroy-stats {:fn destroy-stats :input #{:todo}}
+             :update-stats {:fn update-stats :input #{:todo}}}
+   :emit {:emit {:fn todo-emit :input #{:new-todos :deleted-todos :updated-todos 
+                                        :create-stats :destroy-stats :update-stats}}}})
--- app/src/todomvc_pedestal/rendering.cljs
+++ app/src/todomvc_pedestal/rendering.cljs
@@ -1,6 +1,5 @@
 (ns todomvc-pedestal.rendering
   (:require [domina :as dom]
-            [domina.css :as dom-css]
             [domina.events :as dom-events]
             [io.pedestal.app.render.push :as render]
             [io.pedestal.app.messages :as msg]
@@ -8,136 +7,76 @@
             [io.pedestal.app.render.push.templates :as templates]
             [io.pedestal.app.render.push.handlers.automatic :as d]
             [dommy.core :as dommy])
-  (:require-macros [todomvc-pedestal.html-templates :as html-templates])
-  (:use-macros [dommy.macros :only [sel sel1 node]]))
+  (:use-macros [dommy.macros :only [node]]))
 
-;; Load templates.
 
 (def enter-key-code 13)
 
-(def templates (html-templates/todomvc-pedestal-templates))
 
-;; The way rendering is handled below is the result of using the
-;; renderer provided in `io.pedestal.app.render`. The only requirement
-;; for a renderer is that it must implement the Renderer protocol.
-;;
-;; This renderer dispatches to rendering functions based on the
-;; requested change. See the render-config table below. Each render
-;; function takes three arguments: renderer, render operation and a
-;; a transmitter which is used to send data back to the application's
-;; behavior. This example does not use the transmitter.
-
-(defn render-page [renderer [_ path] transmitter]
-  (let [;; The renderer that we are using here helps us map changes to
-        ;; the UI tree to the DOM. It keeps a mapping of paths to DOM
-        ;; ids. The `get-parent-id` function will return the DOM id of
-        ;; the parent of the node at path. If the path is [:a :b :c]
-        ;; then this will find the id associated with [:a :b]. The
-        ;; root node [] is configured when we created the renderer.
-        parent (render/get-parent-id renderer path)
-        ;; Use the `new-id!` function to associate a new id to the
-        ;; given path. With two arguments, this function will generate
-        ;; a random unique id. With three arguments, the given id will
-        ;; be associated with the given path.
-        id (render/new-id! renderer path)
-        ;; Get the dynamic template named :todomvc-pedestal-page
-        ;; from the templates map. The `add-template` function will
-        ;; associate this template with the node at
-        ;; path. `add-template` returns a function that can be called
-        ;; to generate the initial HTML.
-        html (templates/add-template renderer path (:todomvc-pedestal-page templates))]
-    ;; Call the `html` function, passing the initial values for the
-    ;; template. This returns an HTML string which is then added to
-    ;; the DOM using Domina.
-    (dom/append! (dom/by-id parent) (html {:id id :message ""}))))
-
-(defn render-message [renderer [_ path _ new-value] transmitter]
-  ;; This function responds to a :value event. It uses the
-  ;; `update-t` function to update the template at `path` with the new
-  ;; values in the passed map.
-  (templates/update-t renderer path {:message new-value}))
-
-;; The data structure below is used to map rendering data to functions
-;; which handle rendering for that specific change. This function is
-;; referenced in config/config.clj and must be a function in order to
-;; be used from the tool's "render" view.
-
-(defn render-page [renderer [_ path] transmitter]
-  (let [;; The renderer that we are using here helps us map changes to
-        ;; the UI tree to the DOM. It keeps a mapping of paths to DOM
-        ;; ids. The `get-parent-id` function will return the DOM id of
-        ;; the parent of the node at path. If the path is [:a :b :c]
-        ;; then this will find the id associated with [:a :b]. The
-        ;; root node [] is configured when we created the renderer.
-        parent (render/get-parent-id renderer path)
-        ;; Use the `new-id!` function to associate a new id to the
-        ;; given path. With two arguments, this function will generate
-        ;; a random unique id. With three arguments, the given id will
-        ;; be associated with the given path.
-        id (render/new-id! renderer path)
-        ;; Get the dynamic template named :todomvc-pedestal-page
-        ;; from the templates map. The `add-template` function will
-        ;; associate this template with the node at
-        ;; path. `add-template` returns a function that can be called
-        ;; to generate the initial HTML.
-        html (templates/add-template renderer path (:todomvc-pedestal-page templates))]
-    ;; Call the `html` function, passing the initial values for the
-    ;; template. This returns an HTML string which is then added to
-    ;; the DOM using Domina.
-    (dom/append! (dom/by-id parent) (html {:id id :message ""}))))
-
-(comment defn render-todos [r [_ _ old-value new-value] input-queue]
-  (let [container (dom/by-id "todo-list")]
-    (dom/destroy-children! container)
-    (doseq [new-todo new-value]
-      (dom/append! container
-                   (str "<li>" new-todo "</li>")))))
-
-(defn render-todos [r [_ _ old-value new-value] input-queue]
-  (let [container (sel1 :#todo-list)]
-    (dom/destroy-children! container)
-    (doseq [new-todo new-value]
-      (dommy/append! container
-                     (node [:li [:div.view {:data-todo-id 1}
-                                 [:input.toggle {:type "checkbox"}]
-                                 [:label new-todo]
-                                 [:button.destroy]
-                                 [:input.edit {:id (str "input_" 1)}]]])))))
+(defn new-todo-keypress-handler [e]
+  (let [text-node (dom/by-id "new-todo")
+        text (.-value text-node)
+        evt (.-evt e)
+        char (or (== (.-charCode evt) 0) 
+                 (.fromCharCode js/String (.-charCode evt)))]
+    (set! (.-value text-node) (str text char))
+    (when (= enter-key-code (.-keyCode evt))
+      (set! (.-value text-node) "")
+      [{msg/topic :todo msg/type :add-todo :value text}])))
 
 
 (defn render-page-todo [r [_ _ old-value new-value] input-queue]
   (let [btn (dom/by-id "new-todo")]
     (.focus (dom/by-id "new-todo"))
-    (.log js/console "render-page-todo")
-    (events/send-on :keypress
-                    btn
-                    input-queue
-                    (fn [e]
-                      (let [text-node (dom/by-id "new-todo")
-                            text (.-value text-node)
-                            evt (.-evt e)
-                            char (or (== (.-charCode evt) 0) 
-                                     (.fromCharCode js/String (.-charCode evt)))]
-                        (set! (.-value text-node) (str text char))
-                        (when (= enter-key-code (.-keyCode evt))
-                          (.log js/console text e char)
-                          (set! (.-value text-node) "")
-                          [{msg/topic :todo msg/type :add-todo :value text}]))))))
+    (events/send-on :keypress btn input-queue new-todo-keypress-handler)))
 
+
+(defn toggle-todo-change-handler [e]
+  (let [evt (.-evt e)
+        checkbox (.-target evt)
+        completed (.-checked checkbox)
+        id (dom/attr checkbox "data-todo-id")
+        text (.-value (dom/by-id (str "input_" id)))]
+    [{msg/topic :todo msg/type :upd-todo :id id :title text :completed completed}]))
+
+
+(defn toggle-all-change-handler [e]
+  (let [evt (.-evt e)
+        target (.-target evt)
+        completed (.-checked target)]
+    [{msg/topic :todo msg/type :toggle-all :completed completed}]))
+
+
+(defn update-todo-keypress-handler [e]
+  (let [evt (.-evt e)
+        node (.-target evt)
+        text (.-value node)
+        id (dom/attr node "data-todo-id")
+        completed (.-checked (dom/by-id (str "chb_" id)))
+        char (or (== (.-charCode evt) 0) 
+                 (.fromCharCode js/String (.-charCode evt)))]
+                        (set! (.-value node) (str text char))
+                        (when (= enter-key-code (.-keyCode evt))
+                          (set! (.-value node) "")                          
+                          (dom/remove-class! (dom/by-id id) "editing")
+                          [{msg/topic :todo msg/type :upd-todo :id id :title text :completed completed}])))
 
 (defn create-todo-node [r [_ path] input-queue]
   (let [id (name (last path))
         container (dom/by-id "todo-list")]
-    (.log js/console "create-todo-node" id)
-
-    (dommy/append! container  (node [:li {:id id} [:div.view {:data-todo-id id}
-                                                   [:input.toggle {:type "checkbox"}]
-                                                   [:label {:id (str "lbl_" id)} "new-todo"]
-                                                   [:button.destroy {:id (str "del_" id)}]]
-                                     [:input.edit {:id (str "input_" id)}]]))
+    (dom/append! container  
+                 (node [:li {:id id} 
+                        [:div.view {:data-todo-id id}
+                         [:input.toggle {:id (str "chb_" id)
+                                         :data-todo-id id 
+                                         :type "checkbox"}]
+                         [:label {:id (str "lbl_" id)} "new-todo"]
+                         [:button.destroy {:id (str "del_" id)}]]
+                        [:input.edit {:id (str "input_" id)
+                                      :data-todo-id id}]]))
     ;; delete-todo event
-    (events/send-on :click
-                    (dom/by-id (str "del_" id))
+    (events/send-on :click 
+                    (dom/by-id (str "del_" id)) 
                     input-queue
                     (fn [e]
                       [{msg/topic :todo msg/type :del-todo :value id}]))
@@ -147,57 +86,94 @@
                    (fn [e] 
                      (dom/add-class! (dom/by-id id) "editing")
                      (.focus (dom/by-id (str "input_" id)))))
-
+    ;; checkbox
+    (events/send-on :change
+                    (dom/by-id (str "chb_" id))
+                    input-queue
+                    toggle-todo-change-handler)
+    ;; toggle all
+    (events/send-on :change
+                    (dom/by-id "toggle-all")
+                    input-queue
+                    toggle-all-change-handler)
+    ;; update todo
     (events/send-on :keypress
                     (dom/by-id (str "input_" id))
                     input-queue
-                    (fn [e]
-                      (let [evt (.-evt e)
-                            node (.-target evt)
-                            text (.-value node)
-                            char (or (== (.-charCode evt) 0) 
-                                     (.fromCharCode js/String (.-charCode evt)))]
-                        (set! (.-value node) (str text char))
-                        (when (= enter-key-code (.-keyCode evt))
-                          (.log js/console text e char)
-                          (set! (.-value node) "")
-                          (dom/remove-class! (dom/by-id id) "editing")
-                          [{msg/topic :todo msg/type :upd-todo :id id :title text :completed false}]))))))
+                    update-todo-keypress-handler)))
 
 (defn update-todo [r [_ path o n] d]
-  (let [id (name (last path))]
-    (.log js/console "update-todo" id n(dom-css/sel (dom/by-id id) "label"))
-    (comment dom/set-text! (dom-css/sel (dom/by-id id) "label") (:title n))
+  (let [id (name (last path))
+        div-node (dom/by-id id)
+        checkbox-node (dom/by-id (str "chb_" id))]
     (dom/set-text! (dom/by-id (str "lbl_" id)) (:title n))
-    (dom/set-value! (dom/by-id (str "input_" id)) (:title n))))
+    (dom/set-value! (dom/by-id (str "input_" id)) (:title n))
+    (if (:completed n)
+      (do
+        (dom/set-attr! checkbox-node "checked" true)
+        (dom/add-class! div-node "completed"))
+      (do (dom/set-attr! checkbox-node "checked" false) 
+          (dom/remove-attr! checkbox-node "checked")
+          (dom/remove-class! div-node "completed")))))
 
 
 (defn delete-todo-node [r [_ path o n] d]
   (let [id (name (last path))]
-    (.log js/console "delete-todo" id)
     (dom/destroy! (dom/by-id id))))
 
 
+
+;; stats functions
+(defn draw-todo-clear [input-queue]
+  (let [button (node [:button#clear-completed "Clear completed"])]
+    ;; clear completed
+    (dom/append! (dom/by-id "footer") button)
+    (events/send-on :click
+                    button
+                    input-queue
+                    (fn [e]
+                      [{msg/topic :todo msg/type :clear-completed}]))))
+
+(defn draw-todo-count [input-queue]
+  (let [remaining (node [:span#todo-count [:strong 0] " items"])]
+1    (dom/append! (dom/by-id "footer") remaining)))
+
+(defn create-stats-node [r [_ path] input-queue]
+  (draw-todo-count input-queue)
+  (draw-todo-clear input-queue)
+  (dom/set-styles! (dom/by-id "footer") {:display "block"}))
+
+(defn destroy-stats-node [r n input-queue]
+  (dom/destroy-children! (dom/by-id "footer"))
+  (dom/set-styles! (dom/by-id "footer") {:display "none"}))
+
+(defn update-stats  [r [_ path o n] d]
+  (let [completed-node (dom/by-id "clear-completed")]
+    ;; stats bar display
+    (when (< 0 (:total-todos n))
+      (dom/set-style! (dom/by-id "footer") {:display "block"}))
+    ;; clear completed button
+    (if (< 0 (:todo-completed n))
+      (do
+        (dom/set-styles! completed-node {:display "block"}) 
+        (dom/set-text! completed-node (str "Clear completed (" (:todo-completed n) ")")))
+      (dom/set-styles! completed-node {:display "none"}))
+    ;; # items left
+    (when (<= 0 (:todo-left n))
+      (dom/set-text! (dom/by-id "todo-count") 
+                     (str (or (:todo-left n) 0) 
+                          (if (= 1 (:todo-left n)) " item" " items") 
+                          " left")))))
+
+
 (defn render-config []
-  [;; All :node-create deltas for the node at
-   ;; :io.pedestal.app/view-example-transform will be rendered by the
-   ;; `render-page` function. The node name
-   ;; :io.pedestal.app/view-example-transform is a default name that is used
-   ;; when we don't provide our own combines and emits. To name your
-   ;; own nodes, create a custom combine or emit in the application's
-   ;; behavior.
-   ;;[:node-create  [:io.pedestal.app/view-todo-transform] render-page]
+  [;; todo list
    [:node-create  [:app :todos] render-page-todo]
-   ;; All :node-destroy deltas for this path will be handled by the
-   ;; library function `d/default-exit`.
-   ;;[:node-destroy   [:io.pedestal.app/view-todo-transform] d/default-exit]
-   ;; All :value deltas for this path will be handled by the
-   ;; function `render-message`.
-   [:value [:io.pedestal.app/view-todo-transform] render-message]
+   ;; todo items
    [:node-create  [:app :todos :*] create-todo-node]
    [:node-destroy  [:app :todos :*] delete-todo-node]
-   [:value [:app :todos :*] update-todo]])
-
-;; In render-config, paths can use wildcard keywords :* and :**. :*
-;; means exactly one segment with any value. :** means 0 or more
-;; elements.
+   [:value [:app :todos :*] update-todo]
+   ;; stats
+   [:node-create [:app :stats] create-stats-node]
+   [:node-destroy [:app :stats] destroy-stats-node]
+   [:value [:app :stats] update-stats]])
